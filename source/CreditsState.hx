6bb182a605e462926395df656da767c9243888dc
sebitamontana <93792473+sebitamontana@users.noreply.github.com>
2022-02-26 17:38:21 -0300

    credits

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -39,7 +39,7 @@ class CreditsState extends MusicBeatState
 		DiscordClient.changePresence("In the Menus", null);
 		#end
 
-		bg = new FlxSprite().loadGraphic(Paths.image('menuDesat'));
+		bg = new FlxSprite().loadGraphic(Paths.image('menuBG'));
 		add(bg);
 
 		grpOptions = new FlxTypedGroup<Alphabet>();
@@ -48,6 +48,8 @@ class CreditsState extends MusicBeatState
 		var pisspoop = [ //Name - Icon name - Description - Link - BG Color
 			['Psych Engine Android Port'],
 			['M.A. Jigsaw',		    'majigsaw',		    'Main Coder of The Port',	 'https://www.youtube.com/channel/UC2Sk7vtPzOvbVzdVTWrribQ',	'0xFFC30085'],
+			['Android Port'],
+			['patricio montana skr',		    'majigsaw',		    'porter',	 'https://www.youtube.com/channel/UCMr5sqTwZ7CiWFGYF3LcSFA',	'0xFFC30085'],
 			['Psych Engine Team'],
 			['Shadow Mario',		'shadowmario',		'Main Programmer of Psych Engine',					'https://twitter.com/Shadow_Mario_',	'0xFFFFDD33'],
 			['RiverOaken',			'riveroaken',		'Main Artist/Animator of Psych Engine',				'https://twitter.com/river_oaken',		'0xFFC30085'],
